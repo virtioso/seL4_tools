a519c5c12073ab27e5a4f1e9633d5c362cde8b75
Hannu Lyytinen <hannu.lyytinen@unikie.com>
2026-04-04 15:20:26 +0300

    elfloader/aarch64: prebuild final hyp tables for EFI handoff

--- elfloader-tool/src/arch-arm/sys_boot.c
+++ elfloader-tool/src/arch-arm/sys_boot.c
@@ -51,7 +51,7 @@ extern void finish_relocation(int offset, void *_dynamic, unsigned int total_off
 extern void flush_dcache_range(uintptr_t start, uintptr_t end);
 void continue_boot(int was_relocated);
 
-#ifdef CONFIG_ARCH_AARCH64
+#if defined(CONFIG_ARCH_AARCH64) && defined(CONFIG_IMAGE_EFI)
 extern void quiesce_hyp_mmu(void);
 #endif
 
@@ -111,7 +111,7 @@ void relocate_below_kernel(void)
 #endif
 }
 
-#if defined(CONFIG_ARCH_AARCH64)
+#if defined(CONFIG_ARCH_AARCH64) && defined(CONFIG_IMAGE_EFI)
 /* L1 block descriptor attributes (must match MAIR in enable_elfloader_map_hyp) */
 #define BLOCK_NORMAL  (0x1 | (4 << 2) | (3 << 8) | (1 << 10)) /* MT_NORMAL, ISH, AF */
 #define BLOCK_DEVICE  (0x1 | (1 << 10) | (1ULL << 53) | (1ULL << 54)) /* MT_DEVICE_nGnRnE, AF, PXN, XN */
@@ -120,8 +120,25 @@ void relocate_below_kernel(void)
 #define GB_SHIFT      30
 #define GB_SIZE       (1ULL << GB_SHIFT)
 #define PUD_ENTRIES   512
+#define KEEP_HEADERS_SIZE BIT(PAGE_BITS)
 
-static void map_1gb_range(uint64_t start, uint64_t end, uint64_t attrs)
+static void clear_hyp_boot_tables(void)
+{
+    for (int i = 0; i < BIT(PGD_BITS); i++) {
+        _boot_pgd_up[i] = 0;
+        _boot_pgd_down[i] = 0;
+    }
+    for (int i = 0; i < BIT(PUD_BITS); i++) {
+        _boot_pud_up[i] = 0;
+        _boot_pud_down[i] = 0;
+    }
+    for (int i = 0; i < BIT(PMD_BITS); i++) {
+        _boot_pmd_up[i] = 0;
+        _boot_pmd_down[i] = 0;
+    }
+}
+
+static void map_1gb_range(uint64_t *pud, uint64_t start, uint64_t end, uint64_t attrs)
 {
     uint64_t block_pa = start & ~(GB_SIZE - 1);
     uint64_t block_end = (end + GB_SIZE - 1) & ~(GB_SIZE - 1);
@@ -129,69 +146,158 @@ static void map_1gb_range(uint64_t start, uint64_t end, uint64_t attrs)
     for (; block_pa < block_end; block_pa += GB_SIZE) {
         unsigned int idx = (unsigned int)(block_pa >> GB_SHIFT);
         if (idx < PUD_ENTRIES) {
-            if (attrs == BLOCK_NORMAL || _boot_pud_elfloader[idx] == 0) {
-                _boot_pud_elfloader[idx] = block_pa | attrs;
+            if (attrs == BLOCK_NORMAL || pud[idx] == 0) {
+                pud[idx] = block_pa | attrs;
             }
         }
     }
 }
 
-static void build_elfloader_map(void const *dtb_addr)
+static int build_kernel_boot_info(struct image_info *kernel_boot_info)
 {
-    for (int i = 0; i < BIT(PGD_BITS); i++)
-        _boot_pgd_up[i] = 0;
-    for (int i = 0; i < BIT(PUD_BITS); i++)
-        _boot_pud_elfloader[i] = 0;
-
-    _boot_pgd_up[0] = (uint64_t)(uintptr_t)_boot_pud_elfloader | TABLE_DESC;
-
-    map_1gb_range((uint64_t)(uintptr_t)_text,
-                  (uint64_t)(uintptr_t)_end,
-                  BLOCK_NORMAL);
-
-    /* Map physical destinations for all ELF images in the CPIO archive.
-     * Kernel uses physical addresses; user images are placed after it. */
     void const *cpio = _archive_start;
     size_t cpio_len = _archive_start_end - _archive_start;
-    uint64_t next_phys = 0;
-    const char *name;
+    unsigned long cpio_file_size = 0;
+    void const *elf_blob = cpio_get_file(cpio, cpio_len, "kernel.elf", &cpio_file_size);
+    uint64_t kernel_phys_start, kernel_phys_end;
+    uint64_t kernel_virt_start, kernel_virt_end;
 
-    for (unsigned int idx = 0; ; idx++) {
-        void const *entry = cpio_get_entry(cpio, cpio_len, idx, &name, NULL);
-        if (!entry || !name)
+    UNUSED_VARIABLE(cpio_file_size);
+
+    if (elf_blob == NULL) {
+        printf("ERROR: No kernel image present in archive\n");
+        return -1;
+    }
+    if (elf_checkFile(elf_blob) != 0) {
+        printf("ERROR: Kernel image not a valid ELF file\n");
+        return -1;
+    }
+    if (elf_getMemoryBounds(elf_blob, 1, &kernel_phys_start, &kernel_phys_end) != 1) {
+        printf("ERROR: Could not get kernel physical memory bounds\n");
+        return -1;
+    }
+    if (elf_getMemoryBounds(elf_blob, 0, &kernel_virt_start, &kernel_virt_end) != 1) {
+        printf("ERROR: Could not get kernel virtual memory bounds\n");
+        return -1;
+    }
+
+    kernel_boot_info->phys_region_start = (paddr_t)kernel_phys_start;
+    kernel_boot_info->phys_region_end = (paddr_t)ROUND_UP(kernel_phys_end, PAGE_BITS);
+    kernel_boot_info->virt_region_start = (vaddr_t)kernel_virt_start;
+    kernel_boot_info->virt_region_end = (vaddr_t)ROUND_UP(kernel_virt_end, PAGE_BITS);
+    kernel_boot_info->virt_entry = (vaddr_t)elf_getEntryPoint(elf_blob);
+    kernel_boot_info->phys_virt_offset = kernel_boot_info->phys_region_start - kernel_boot_info->virt_region_start;
+    return 0;
+}
+
+static void map_user_image_destinations(void const *cpio, size_t cpio_len, paddr_t next_phys_addr)
+{
+    const char *elf_filename;
+    unsigned int user_elf_offset = 2;
+
+    cpio_get_entry(cpio, cpio_len, 0, &elf_filename, NULL);
+    if (!elf_filename || strcmp(elf_filename, "kernel.elf") != 0) {
+        printf("ERROR: Kernel image not first image in archive\n");
+        abort();
+    }
+
+    cpio_get_entry(cpio, cpio_len, 1, &elf_filename, NULL);
+    if (!elf_filename || strcmp(elf_filename, "kernel.dtb") != 0) {
+        user_elf_offset = 1;
+    }
+
+    for (unsigned int i = 0; i < 1; i++) {
+        unsigned long cpio_file_size = 0;
+        void const *user_elf = cpio_get_entry(cpio, cpio_len, i + user_elf_offset,
+                                              &elf_filename, &cpio_file_size);
+        uint64_t min_vaddr, max_vaddr;
+        size_t image_size;
+        paddr_t mapped_end;
+
+        UNUSED_VARIABLE(cpio_file_size);
+
+        if (user_elf == NULL) {
             break;
-        if (elf_checkFile(entry) != 0)
-            continue;
-
-        uint64_t lo, hi;
-        int is_kernel = (strcmp(name, "kernel.elf") == 0);
-
-        if (elf_getMemoryBounds(entry, is_kernel, &lo, &hi) != 1)
-            continue;
-
-        if (is_kernel) {
-            map_1gb_range(lo, hi, BLOCK_NORMAL);
-            next_phys = hi;
-        } else {
-            uint64_t size = hi - lo;
-            map_1gb_range(next_phys, next_phys + size, BLOCK_NORMAL);
-            next_phys += size;
         }
+        if (elf_getMemoryBounds(user_elf, 0, &min_vaddr, &max_vaddr) != 1) {
+            printf("ERROR: Could not get user image bounds\n");
+            abort();
+        }
+
+        max_vaddr = ROUND_UP(max_vaddr, PAGE_BITS);
+        image_size = (size_t)(max_vaddr - min_vaddr);
+        mapped_end = ROUND_UP(next_phys_addr + image_size, PAGE_BITS) + KEEP_HEADERS_SIZE;
+        map_1gb_range(_boot_pud_down, next_phys_addr, mapped_end, BLOCK_NORMAL);
+        next_phys_addr = mapped_end;
+    }
+}
+
+static void init_prebuilt_hyp_boot_vspace(struct image_info *kernel_boot_info, void const *dtb_addr)
+{
+    void const *cpio = _archive_start;
+    size_t cpio_len = _archive_start_end - _archive_start;
+    uintptr_t loader_start = (uintptr_t)_text;
+    uintptr_t loader_end = (uintptr_t)_end;
+    uintptr_t loader_size = loader_end - loader_start;
+    paddr_t next_phys_addr = kernel_boot_info->phys_region_end;
+    word_t pmd_index;
+
+    clear_hyp_boot_tables();
+
+    _boot_pgd_down[0] = (uint64_t)(uintptr_t)_boot_pud_down | TABLE_DESC;
+    _boot_pgd_down[GET_PGD_INDEX(kernel_boot_info->virt_region_start)]
+        = (uint64_t)(uintptr_t)_boot_pud_up | TABLE_DESC;
+    _boot_pud_up[GET_PUD_INDEX(kernel_boot_info->virt_region_start)]
+        = (uint64_t)(uintptr_t)_boot_pmd_up | TABLE_DESC;
+
+    pmd_index = GET_PMD_INDEX(kernel_boot_info->virt_region_start);
+    for (word_t i = pmd_index; i < BIT(PMD_BITS); i++) {
+        _boot_pmd_up[i] = (((i - pmd_index) << ARM_2MB_BLOCK_BITS) + kernel_boot_info->phys_region_start)
+                          | BIT(10)
+#if CONFIG_MAX_NUM_NODES > 1
+                          | (3 << 8)
+#endif
+                          | (4 << 2)
+                          | BIT(0);
+    }
+
+    map_1gb_range(_boot_pud_down, loader_start, loader_end, BLOCK_NORMAL);
+    map_1gb_range(_boot_pud_down,
+                  kernel_boot_info->phys_region_start,
+                  kernel_boot_info->phys_region_end,
+                  BLOCK_NORMAL);
+
+    if (loader_end > kernel_boot_info->virt_region_start) {
+        uintptr_t relocated_start = kernel_boot_info->virt_region_start - ROUND_UP(loader_size, MAX_ALIGN_BITS);
+        map_1gb_range(_boot_pud_down, relocated_start, relocated_start + loader_size, BLOCK_NORMAL);
     }
 
     if (dtb_addr) {
-        map_1gb_range((uint64_t)(uintptr_t)dtb_addr,
-                      (uint64_t)(uintptr_t)dtb_addr + 1,
-                      BLOCK_NORMAL);
+        size_t dtb_bytes = fdt_size(dtb_addr);
+        if (dtb_bytes == 0) {
+            printf("ERROR: Invalid device tree blob supplied\n");
+            abort();
+        }
+
+        map_1gb_range(_boot_pud_down, (uint64_t)(uintptr_t)dtb_addr,
+                      (uint64_t)(uintptr_t)dtb_addr + dtb_bytes, BLOCK_NORMAL);
+
+        next_phys_addr = ROUND_UP(next_phys_addr, PAGE_BITS);
+        map_1gb_range(_boot_pud_down, next_phys_addr, next_phys_addr + dtb_bytes, BLOCK_NORMAL);
+        next_phys_addr = ROUND_UP(next_phys_addr + dtb_bytes, PAGE_BITS);
+    } else {
+        next_phys_addr = ROUND_UP(next_phys_addr, PAGE_BITS);
     }
+
+    map_user_image_destinations(cpio, cpio_len, next_phys_addr);
 
     volatile void *uart_mmio = uart_get_mmio();
     if (uart_mmio) {
         uint64_t uart_pa = (uint64_t)(uintptr_t)uart_mmio;
-        map_1gb_range(uart_pa, uart_pa + 1, BLOCK_DEVICE);
+        map_1gb_range(_boot_pud_down, uart_pa, uart_pa + 1, BLOCK_DEVICE);
     }
 }
-#endif /* CONFIG_ARCH_AARCH64 */
+#endif /* CONFIG_ARCH_AARCH64 && CONFIG_IMAGE_EFI */
 
 /*
  * Entry point.
@@ -235,11 +341,16 @@ void main(UNUSED void *arg)
 
 #endif
 
-#if defined(CONFIG_ARCH_AARCH64)
+#if defined(CONFIG_ARCH_AARCH64) && defined(CONFIG_IMAGE_EFI)
     if (is_hyp_mode()) {
+        struct image_info kernel_boot_info;
+
+        if (build_kernel_boot_info(&kernel_boot_info) != 0) {
+            abort();
+        }
         quiesce_hyp_mmu();
-        build_elfloader_map(bootloader_dtb);
-        arm_enable_hyp_mmu(_boot_pgd_up);
+        init_prebuilt_hyp_boot_vspace(&kernel_boot_info, bootloader_dtb);
+        arm_enable_hyp_mmu(_boot_pgd_down);
     }
 #endif
 
@@ -359,15 +470,20 @@ void continue_boot(int was_relocated)
 #ifdef CONFIG_ARCH_AARCH64
     flush_dcache_range(kernel_info.phys_region_start, kernel_info.phys_region_end);
     flush_dcache_range(user_info.phys_region_start, user_info.phys_region_end);
+    if (dtb && dtb_size) {
+        flush_dcache_range((uintptr_t)dtb, (uintptr_t)dtb + dtb_size);
+    }
 #endif
 
     /* Setup MMU. */
     if (is_hyp_mode()) {
-#ifdef CONFIG_ARCH_AARCH64
+#if defined(CONFIG_ARCH_AARCH64) && !defined(CONFIG_IMAGE_EFI)
         extern void disable_caches_hyp();
         disable_caches_hyp();
-#endif
         init_hyp_boot_vspace(&kernel_info);
+#elif !defined(CONFIG_ARCH_AARCH64)
+        init_hyp_boot_vspace(&kernel_info);
+#endif
     } else {
         /* If we are not in HYP mode, we enable the SV MMU and paging
          * just in case the kernel does not support hyp mode. */
